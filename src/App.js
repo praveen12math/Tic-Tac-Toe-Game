70ca6bd59e682cea37e257cdf46ed63d5e3b1f8a
praveen12math <praveen12math@github.com>
2020-06-29 12:22:14 +0530

    second commit 1.2

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ function App() {
     </Row>
     </Container> <br/><br/>
     <div className="devel" style={{textAlign:"center"}}>
-    Developed by Praveen Yadav with React js <img className="logoimg" src="logo192.png" />
+    Developed by Praveen Yadav with React js <img className="logoimg" src="logo192.png" alt="" />
     </div>
     </div>
   );
